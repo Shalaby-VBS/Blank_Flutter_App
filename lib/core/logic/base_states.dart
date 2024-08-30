e12ecd97ce1c5275448c7346ce5b06324adadbdf
Shalaby-VBS <shalaby.vbs@gmail.com>
2024-08-30 16:23:49 +0300

    Shalaby-VBS

--- lib/core/logic/base_states.dart
+++ lib/core/logic/base_states.dart
@@ -4,6 +4,10 @@ class Initial extends BaseStates {}
 
 class Loading extends BaseStates {}
 
-class Success extends BaseStates {}
+class Success extends BaseStates {
+  final dynamic data;
+
+  Success(this.data);
+}
 
 class Error extends BaseStates {}
