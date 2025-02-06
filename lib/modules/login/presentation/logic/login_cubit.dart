e8bfe2fb72256b1fa7bf9486ad2cd0b842e8372e
Shalaby-VBS <shalaby.vbs@gmail.com>
2025-02-06 14:58:32 +0200

    Shalaby-VBS

--- lib/modules/login/presentation/logic/login_cubit.dart
+++ lib/modules/login/presentation/logic/login_cubit.dart
@@ -14,7 +14,6 @@ class LoginCubit extends Cubit<LoginState> {
   final formKey = GlobalKey<FormState>();
 
   final emailController = TextEditingController();
-
   final passwordController = TextEditingController();
   bool isPasswordVisible = false;
 
